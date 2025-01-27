ec2fb520c85b0f7889a5edbb69a0d6c156f9f497
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2025-01-27 22:00:12 +0000

    add more output and fixes to train script

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from transformers import Trainer, TrainingArguments
 from datasets import ScalpelDataset
 
 
-def train(train_dataset, val_dataset):
+def train(model, feature_extractor, tokenizer, train_dataset, val_dataset):
     training_args = TrainingArguments(
         output_dir='./results',
         num_train_epochs=3,
@@ -36,13 +36,23 @@ if __name__ == '__main__':
     feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
     tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
+    print(f'Model: {model}')
+    print(f'Feature extractor: {feature_extractor}')
+    print(f'Tokenizer: {tokenizer}')
+
     dataset, test_dataset = ScalpelDataset('../scalpel-angles/scalpel_dataset.csv', feature_extractor, tokenizer)
     train_dataset, val_dataset = dataset.train_val_split(0.9)
+    print(f'Train dataset length: {len(train_dataset)}')
+    print(f'Validation dataset length: {len(val_dataset)}')
+    print(f'Test dataset length: {len(test_dataset)}')
 
     model, feature_extractor, tokenizer =  train(model, feature_extractor, tokenizer, train_dataset, val_dataset)
 
+    print(f'Model trained')
+
     model.eval()
 
+    print(f'Testing model on test dataset')
     for image, caption in test_dataset:
         inputs = feature_extractor(image, return_tensors="pt")
         outputs = model.generate(**inputs, max_new_tokens=20)
