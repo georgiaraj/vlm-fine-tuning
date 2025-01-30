7dffadf79cd5a9e4ca8ae2ca340acc6910788350
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2025-01-30 21:12:30 +0000

    Lots of fixes to allow the dataset to be split into train and test

--- scripts/convert_scalpel_data_to_captions.py
+++ scripts/convert_scalpel_data_to_captions.py
@@ -1,63 +1,77 @@
 import pdb
+import csv
+import argparse
 from PIL import Image
 from PIL import ImageDraw
 import math
 import json
 from pathlib import Path
 
-root_dir = Path('../scalpel-angles')
-image_dir = root_dir / 'images'
-json_dir = root_dir / 'jsons'
 
-images = list(image_dir.rglob('*.png'))
-jsons = [f.name for f in json_dir.rglob('*.json')]
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', type=str, default='scalpel-angles')
+    return parser.parse_args()
 
-draw_images = False
 
-with open(str(root_dir / 'scalpel_dataset.csv'), 'w') as f:
-    f.write('image, caption, instruments, bboxes, angle\n')
+if __name__ == '__main__':
+    args = get_args()
 
-    for im in images:
-        json_file = im.parent.parent / 'jsons' / (im.name.replace('.png', '.json'))
+    root_dir = Path(args.dataset_path)
+    image_dir = root_dir / 'images'
+    json_dir = root_dir / 'jsons'
 
-        if json_file.name not in jsons:
-            continue
+    images = list(image_dir.rglob('*.png'))
+    jsons = [f.name for f in json_dir.rglob('*.json')]
 
-        with open(str(json_file), 'r') as j_file:
-            data = json.load(j_file)
+    draw_images = False
 
-        angle = data['image']['platform_rotation']
+    all_orders = []
+    data_set = 'train'
 
-        if draw_images:
-            img = Image.open(str(im))
-            draw = ImageDraw.Draw(img)
+    with open(str(root_dir / 'scalpel_dataset.csv'), 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['image', 'caption', 'data_set', 'instruments', 'bboxes', 'angle'])
 
-        def find_order(instrument, angle):
-            if angle < 90 or angle > 270:
-                return instrument['bbox'][0]
-            elif angle == 90:
-                return instrument['bbox'][1]
-            elif angle == 270:
-                return -instrument['bbox'][1]
-            else:
-                return -instrument['bbox'][0]
+        for im in images:
+            json_file = im.parent.parent / 'jsons' / (im.name.replace('.png', '.json'))
 
-        # Work out the order of the instruments
-        order = sorted(data['instruments'], key=lambda x: find_order(x, angle))
+            if json_file.name not in jsons:
+                continue
 
-        f.write(f'{im.name},"This image shows the following instruments: {", ".join([i["name"] for i in order])} at a {angle} degree angle.", ')
+            with open(str(json_file), 'r') as j_file:
+                data = json.load(j_file)
 
-        for i in data['instruments']:
-            f.write(f'[{",".join([i["name"] for i in data["instruments"]])}], [{",".join([",".join([str(b) for b in i["bbox"]]) for i in data["instruments"]])}],' 
-                    f'{angle},')
+            angle = data['image']['platform_rotation']
 
             if draw_images:
-                draw.rectangle([i["bbox"][0], i["bbox"][1], i["bbox"][0] + i["bbox"][2], i["bbox"][1] + i["bbox"][3]], outline="red")
-        if draw_images:
-            img.save(im.name.replace('.png', '_annotated.png'))
+                img = Image.open(str(im))
+                draw = ImageDraw.Draw(img)
 
-        f.write('\n')
-        # Check for straight bboxes    
-        #if data['angle'] == 0:
-        #    f.write(f'{im.name}, {i["name"]}, {",".join([str(b) for b in i["bbox"]])}\n')
-print(f'CSV file created at {root_dir / "scalpel_dataset.csv"}')
+            def find_order(instrument, angle):
+                if angle < 90 or angle > 270:
+                    return instrument['bbox'][0]
+                elif angle == 90:
+                    return instrument['bbox'][1]
+                elif angle == 270:
+                    return -instrument['bbox'][1]
+                else:
+                    return -instrument['bbox'][0]
+
+            # Work out the order of the instruments
+            order = sorted(data['instruments'], key=lambda x: find_order(x, angle))
+
+            instruments = [i['name'].replace('"', '') for i in order]
+            bboxes = [[i['bbox'] for i in order]]
+            if len(set(all_orders)) == 28 and instruments not in all_orders:
+                data_set = 'test'
+
+            all_orders.append(','.join(instruments))
+
+            writer.writerow([im.name, f'This image shows the following instruments: {", ".join(instruments)} at a {angle} degree angle.', data_set, instruments, bboxes, angle])
+
+            if draw_images:
+                img.save(im.name.replace('.png', '_annotated.png'))
+
+    print(f'CSV file created at {root_dir / "scalpel_dataset.csv"}')    
+
