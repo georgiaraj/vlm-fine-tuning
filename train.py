68f7c518378dcfa59447fb5bb49260d5ea5f3cfc
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2025-01-30 21:13:28 +0000

    fixes to train script

--- train.py
+++ train.py
@@ -1,13 +1,39 @@
+import pdb
 import torch
+import argparse
+from pathlib import Path
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 from transformers import Trainer, TrainingArguments
+from evaluate import load
 
-from datasets import ScalpelDataset
+from custom_datasets import ScalpelDataset
 
 
-def train(model, feature_extractor, tokenizer, train_dataset, val_dataset):
+rouge_score = load('rouge')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str, default='nlpconnect/vit-gpt2-image-captioning')
+    parser.add_argument('--dataset_path', type=str, default='scalpel-angles/scalpel_dataset.csv')
+    parser.add_argument('--output_dir', type=str, default='scalpel-angles-model')
+    return parser.parse_args()
+
+
+
+
+def train(model, feature_extractor, tokenizer, train_dataset, val_dataset, args):
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        return rouge_score.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    
+
     training_args = TrainingArguments(
-        output_dir='./results',
+        output_dir=args.output_dir,
         num_train_epochs=3,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
@@ -21,32 +47,42 @@ def train(model, feature_extractor, tokenizer, train_dataset, val_dataset):
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=val_dataset,
+        compute_metrics=compute_metrics
     )
 
     trainer.train()
 
-    model.save_pretrained('scalpel-angles-model')
-    feature_extractor.save_pretrained('scalpel-angles-feature-extractor')
-    tokenizer.save_pretrained('scalpel-angles-tokenizer')
+    model.save_pretrained(args.output_dir)
+    feature_extractor.save_pretrained(args.output_dir)
+    tokenizer.save_pretrained(args.output_dir)
 
     return model, feature_extractor, tokenizer
 
 if __name__ == '__main__':
-    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-    feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+    args = parse_args()
+
+    feature_extractor = ViTImageProcessor.from_pretrained(args.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    
+    data_dir = Path(args.dataset_path)
+
+    dataset = ScalpelDataset(data_dir, feature_extractor, tokenizer)
+    test_dataset = ScalpelDataset(data_dir, feature_extractor, tokenizer, data_set='test')
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.9, 0.1])
+    print(f'Train dataset length: {len(train_dataset)}')
+    print(f'Validation dataset length: {len(val_dataset)}')
+    print(f'Test dataset length: {len(test_dataset)}')
+
+    model = VisionEncoderDecoderModel.from_pretrained(args.model_name)
 
     print(f'Model: {model}')
     print(f'Feature extractor: {feature_extractor}')
     print(f'Tokenizer: {tokenizer}')
 
-    dataset, test_dataset = ScalpelDataset('../scalpel-angles/scalpel_dataset.csv', feature_extractor, tokenizer)
-    train_dataset, val_dataset = dataset.train_val_split(0.9)
-    print(f'Train dataset length: {len(train_dataset)}')
-    print(f'Validation dataset length: {len(val_dataset)}')
-    print(f'Test dataset length: {len(test_dataset)}')
+    pdb.set_trace()
 
-    model, feature_extractor, tokenizer =  train(model, feature_extractor, tokenizer, train_dataset, val_dataset)
+    model, feature_extractor, tokenizer =  train(model, feature_extractor, tokenizer, train_dataset, val_dataset, args)
 
     print(f'Model trained')
 
